7d3f0c1c60e61bb8f3a06b2a9d08899af1f394c2
cancarlose <carlosedlima4@gmail.com>
2023-11-27 15:57:24 -0300

    alterações salvas

---
+++ src/JS/script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  
+  // subistituir imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./src/assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./src/assets/avatar.png")
+  }
+}
